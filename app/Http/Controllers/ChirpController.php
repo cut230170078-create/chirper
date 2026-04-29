a4b40a4c9e2fd22a19af5acfc19c7ad1fa938649
Cut Aqsa <cut.230170078@mhs.unimal.ac.id>
2026-04-29 22:45:26 +0700

    fix controller syntax

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -1,21 +1,32 @@
-
-
 <?php
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ChirpController extends Controller
-{
-    public function index()
-    {
-        $chirps = [
-            "Halo Laravel",
-            "Belajar Controller",
-            "Deploy sukses 🚀"
-        ];
+class ChirpController extends Controller {
 
-        return view('welcome', ['chirps' => $chirps]);
-    }
+    public function index()
+{
+    $chirps = [
+        [
+            'author' => 'Aqsa',
+            'message' => 'Halo Laravel',
+            'time' => '1 menit lalu'
+        ],
+        [
+            'author' => 'Aqsa',
+            'message' => 'Belajar Controller',
+            'time' => '5 menit lalu'
+        ],
+        [
+            'author' => 'Aqsa',
+            'message' => 'Deploy sukses 🚀',
+            'time' => '10 menit lalu'
+        ]
+    ];
+
+    return view('welcome', ['chirps' => $chirps]);
+        
+}
 }
